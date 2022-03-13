d799736557c4105b3b707be73770bb9545f43e9a
Krumomir <61614440+Krumomir@users.noreply.github.com>
2022-03-13 04:42:46 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,54 +1,80 @@
-from cProfile import label
-from astroNN.datasets import load_galaxy10
-from tensorflow.keras import utils
+import os
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-import numpy as np
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.applications.vgg16 import VGG16
-import h5py
-
-from tensorflow import keras
-from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from tensorflow import keras
 
+from keras.models import Sequential
+from keras.layers import Conv2D, AveragePooling2D, Flatten, Dense, Dropout
+from keras.callbacks import ReduceLROnPlateau
 
-with h5py.File('Galaxy10_DECals.h5', 'r') as F:
-    images = np.array(F['images'])
-    labels = np.array(F['ans'])
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix
+from tensorflow.keras import utils
 
-# To convert the labels to categorical 10 classes
-labels = utils.to_categorical(labels, 10)
+from astroNN.datasets import galaxy10
+from astroNN.datasets.galaxy10 import galaxy10cls_lookup
 
-# To convert to desirable type
-labels = labels.astype(np.float32)
-images = images.astype(np.float32)
+images, labels = galaxy10.load_data()
 
-print(len(images[0]))
+x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.2)
 
-# # To convert to desirable type
-labels = labels.astype(np.float32)
-images = images.astype(np.float32)
+features = ['Disk, Face-on, No Spiral', 'Smooth, Completely round', 'Smooth, in-between round', 'Smooth, Cigar shaped', 'Disk, Edge-on, Rounded Bulge', 'Disk, Edge-on, Boxy Bulge', 
+            'Disk, Edge-on, No Bulge','Disk, Face-on, Tight Spiral', 'Disk, Face-on, Medium Spiral', 'Disk, Face-on, Loose Spiral']
 
+x_train = x_train / 255.0
+x_test = x_test / 255.0
 
-train_idx, test_idx = train_test_split(np.arange(labels.shape[0]), test_size=0.1)
-train_images, train_labels, test_images, test_labels = images[train_idx], labels[train_idx], images[test_idx], labels[test_idx]
+x_train.shape, x_test.shape
 
+fig = plt.figure(figsize=(20,20)) 
 
+for i in range(25):
+    plt.subplot(5,5,i+1)    
+    plt.imshow(x_train[i])
+    plt.title(features[y_train[i]])
+    fig.tight_layout(pad=3.0)
+    
+plt.show()
 
-vgg16_model = tf.keras.applications.vgg16.VGG16()
+df = pd.DataFrame(data=labels)
+
+counts = df.value_counts().sort_index()
+print(counts)
+
+def class_distribution(x, y, labels):
+    fig, ax = plt.subplots()
+    ax.bar(x, y)
+    ax.set_xticklabels(labels, rotation=90)
+    plt.show()
+    
+class_distribution(features, counts, features)
+
 model = Sequential()
 
-for layer in vgg16_model.layers[:-1]:
-  model.add(layer)
-for layer in model.layers:
-  layer.trainable = False
-model.add(Dense(units=10, activation='softmax'))
-model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-model.summary()
-model.fit(x = images[train_idx], y = labels[train_idx], epochs = 1)
+# Baseline model to compare to LeNet-5
+model.add(Flatten(input_shape=(69, 69, 3)))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+model.save_weights(checkpoint_path.format(epoch=9))
+
+model_optimizer = keras.optimizers.Adam(lr=0.001)
+
+model.compile(optimizer=model_optimizer, loss='sparse_categorical_crossentropy', metrics=["accuracy"])
+reduceLR = ReduceLROnPlateau(monitor='accuracy', factor=.001, patience=1, min_delta=0.01, mode="auto")
+lol = model.fit(x_train, y_train, epochs=10, callbacks=[reduceLR])
+
+lassification_report(y_test, predict)
+
+matrix = confusion_matrix(y_test, predict)
+sns.heatmap(matrix, annot=True)
+plt.title('Galaxy Confusion Matrix')
+plt.xlabel('Predicted class')
+plt.ylabel('True class')
